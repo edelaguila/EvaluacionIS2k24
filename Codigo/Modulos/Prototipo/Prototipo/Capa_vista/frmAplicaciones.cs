46df1d62b8cd7d9ae7553e39045809b9f84a773c
D4nnny <dperezf5@miumg.edu.gt>
2024-02-29 10:09:42 -0600

    Agregando Diagramas

--- Codigo/Modulos/Prototipo/Prototipo/Capa_vista/frmAplicaciones.cs
+++ Codigo/Modulos/Prototipo/Prototipo/Capa_vista/frmAplicaciones.cs
@@ -21,14 +21,7 @@ namespace Vista_PrototipoMenu
 
         private void navegador1_Load(object sender, EventArgs e)
         {
-            NavegadorVista.Navegador.idApp = "1002";  //código aplicación asignado al formulario
-            navegador1.actual = this;
-            navegador1.tabla = dataGridView1;
-            TextBox[] Grupotextbox = { txtId, txtNombre, txtDescripcion, txtEstado };
-            TextBox[] Idtextbox = { txtId, txtNombre };
-            navegador1.textbox = Grupotextbox;
-            navegador1.textboxi = Idtextbox;
-            navegador1.cargar(dataGridView1, Grupotextbox, cn.getNombreBd());
+            
 
         }
     }
